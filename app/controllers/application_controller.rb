e1b375625f0b31a65aab7cdc10232bb5bc85781d
Tanner <troxs690@gmail.com>
2022-11-02 12:30:58 -0500

    created tables

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,36 @@
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
-  # Add your routes here
-  
+  get '/employees' do
+    employee = Employee.All
+    employee.to_json
+end
+
+post '/employees' do
+  employee = Employee.create(
+    first_name: params[:first_name]
+    last_name: params[:last_name]
+    position: params[:position]
+    salary: params[:salary]
+    company_id: params[:company_id]
+  )
+  employee.to_json
+end
+
+patch '/employees/:id' do
+  updateEmployee = Employee.find(params[:id])
+
+  update = params.select do |key, value|
+    ["first_name", "last_name", "position", "salary", "company_id" ].include?(key)
+  end
+  updateEmployee.update(update)
+
+  updateEmployee.to_json
+end
+
+delete '/employee/:id' do
+  deleteEmployee = Employee.find(params[:id])
+  deleteEmployee.destroy
+  deleteEmployee.to_json
+end
 end
